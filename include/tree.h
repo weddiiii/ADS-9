c61f87f43a681e3284eca812747fbac9b83d2083
weddiiii <150699840+weddiiii@users.noreply.github.com>
2026-06-07 23:29:36 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,22 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+
+class PMTree {
+ private:
+  std::vector<char> alphabet;
+
+ public:
+  explicit PMTree(const std::vector<char>& data) : alphabet(data) {}
+
+  std::vector<char> getAlphabet() const {
+    return alphabet;
+  }
+};
+
+std::vector<std::vector<char>> setAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
 #endif  // INCLUDE_TREE_H_
