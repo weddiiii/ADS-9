1bac08b3b9e21fe3cd10a1aba6408e86ee182a4c
weddiiii <150699840+weddiiii@users.noreply.github.com>
2026-06-07 23:34:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 // Copyright 2022 NNTU-CS
+
+#include <iostream>
+#include <vector>
+
 #include "tree.h"
 
 int main() {
+  PMTree tree(std::vector<char>{'1', '2', '3', '4'});
+
+  std::vector<std::vector<char>> perms = getAllPerms(tree);
+
+  std::cout << "Count = " << perms.size() << std::endl;
+
+  std::vector<char> p1 = getPerm1(tree, 1);
+
+  for (char c : p1)
+    std::cout << c;
+
+  std::cout << std::endl;
+
+  std::vector<char> p2 = getPerm2(tree, 2);
+
+  for (char c : p2)
+    std::cout << c;
+
+  std::cout << std::endl;
+
   return 0;
 }
